cc2742e61b3ff07fa0d5f022a2413d674ea617cb
flr19 <flr19@scarletmail.rutgers.edu>
2022-10-30 22:56:14 -0400

    Adding points to criterias

--- src/main/java/com/match/matchapi/MatchService.java
+++ src/main/java/com/match/matchapi/MatchService.java
@@ -115,19 +115,75 @@ public class MatchService {
 
     }
     public List<Student> matchStudents (Student student) {
+        //list of all students
+        List<Student> allStudents = new ArrayList<>();
+        allStudents = getAllStudents();
+
+        //points array
+        List<Integer> points = new ArrayList<>();
+
+        //getting students with same course
         Query query = new Query();
         query.addCriteria(Criteria.where("course").is(student.getCourse()));
-        List<Student> students = mongoTemplate.find(query, Student.class);
-        for(int i=0;i<students.size();i++) {
-            if(students.get(i).getUserName().equals(student.getUserName())) {
-                students.remove(i);
+        List<Student> studentsWithSameCourse = mongoTemplate.find(query, Student.class);
+
+        //adding points to all students array
+        // 3 points for campus
+        int indexOfPointsArray;
+        for(int i=0;i<studentsWithSameCourse.size();i++) {
+            if(allStudents.contains(studentsWithSameCourse.get(i))) {
+                indexOfPointsArray = allStudents.indexOf(studentsWithSameCourse.get(i));
+                points.add(indexOfPointsArray,3);
             }
         }
+
+        //campus
+        Query campusQuery = new Query();
+        campusQuery.addCriteria(Criteria.where("campus").is(student.getCampus()));
+        List<Student> studentsWithSameCampus = mongoTemplate.find(campusQuery, Student.class);
+        //2 points for campus
+        for(int i=0;i<studentsWithSameCampus.size();i++) {
+            if(allStudents.contains(studentsWithSameCampus.get(i))) {
+                indexOfPointsArray = allStudents.indexOf(studentsWithSameCampus.get(i));
+                points.add(indexOfPointsArray,points.get(i).intValue()+2);
+            }
+        }
+
+        Query majorQuery = new Query();
+        majorQuery.addCriteria(Criteria.where("major").is(student.getMajor()));
+        List<Student> studentsWithSameMajor = mongoTemplate.find(majorQuery, Student.class);
+        //2 points for major
+        for(int i=0;i<studentsWithSameCampus.size();i++) {
+            if(allStudents.contains(studentsWithSameMajor.get(i))) {
+                indexOfPointsArray = allStudents.indexOf(studentsWithSameMajor.get(i));
+                points.add(indexOfPointsArray,points.get(i).intValue()+2);
+            }
+        }
+
+        Query genderQuery = new Query();
+        genderQuery.addCriteria(Criteria.where("gender").is(student.getGenderPreference()));
+        List<Student> studentsWithSameGender = mongoTemplate.find(genderQuery, Student.class);
+        //2 points for gender
+        for(int i=0;i<studentsWithSameGender.size();i++) {
+            if(allStudents.contains(studentsWithSameGender.get(i))) {
+                indexOfPointsArray = allStudents.indexOf(studentsWithSameGender.get(i));
+                points.add(indexOfPointsArray,points.get(i).intValue()+2);
+            }
+        }
+
+        //NOW MERGESORT
+
+
+//        for(int i=0;i<students.size();i++) {
+//            if(students.get(i).getUserName().equals(student.getUserName())) {
+//                students.remove(i);
+//            }
+//        }
 //       List<String> studentIds = new ArrayList<>();
 //        for(int i=0;i<students.size();i++) {
 //            studentIds.add(students.get(i).getId());
 //        }
 //        return studentIds;
-        return students;
+        //return students;
     }
 }
