b406f1ec849bd56579405abcd1cc1068f911f14a
kuhuhalder <kuhuhalder2701@gmail.com>
2022-11-30 18:28:37 -0500

    fixed cross origins

--- src/main/java/com/match/matchapi/MatchController.java
+++ src/main/java/com/match/matchapi/MatchController.java
@@ -23,14 +23,14 @@ public class MatchController {
 
     @GetMapping(path = "/getAll")
     @ResponseBody
-    @CrossOrigin(origins={"http://localhost:3000", "https://match-swensational.netlify.app/"})
+    @CrossOrigin(origins={"http://localhost:3000", "https://match-swensational.netlify.app"})
     public List<Student> fetchAllStudents() {
         return matchService.getAllStudents();
     }
 
     @Transactional
     @PostMapping(path = "/add")
-    @CrossOrigin(origins={"http://localhost:3000", "https://match-swensational.netlify.app/"})
+    @CrossOrigin(origins={"http://localhost:3000", "https://match-swensational.netlify.app"})
     public ResponseEntity addStudent(@RequestBody Student student){
         if(matchService.addStudent(student)) {
             return ResponseEntity.status(HttpStatus.CREATED).build();
@@ -42,7 +42,7 @@ public class MatchController {
 
     @Transactional
     @PostMapping(path = "/matchAdd")
-    @CrossOrigin(origins={"http://localhost:3000", "https://match-swensational.netlify.app/"})
+    @CrossOrigin(origins={"http://localhost:3000", "https://match-swensational.netlify.app"})
     public ResponseEntity addMatch(@RequestBody Matches match){
         int resultVar = matchService.addMatches(match);
         if(resultVar != -1 && resultVar != 2 && resultVar != 3) {
@@ -54,7 +54,7 @@ public class MatchController {
     }
 
     @GetMapping(path = "/validate/{userName}/{password}")
-    @CrossOrigin(origins={"http://localhost:3000", "https://match-swensational.netlify.app/"})
+    @CrossOrigin(origins={"http://localhost:3000", "https://match-swensational.netlify.app"})
     public ResponseEntity validation(@PathVariable String userName, @PathVariable String password){
         if (matchService.validation(userName, password)){
             return ResponseEntity.ok(true);
@@ -65,14 +65,14 @@ public class MatchController {
 
     @GetMapping(path = "/getStudent/{userName}")
     @ResponseBody
-    @CrossOrigin(origins={"http://localhost:3000", "https://match-swensational.netlify.app/"})
+    @CrossOrigin(origins={"http://localhost:3000", "https://match-swensational.netlify.app"})
     public Student getStudent(@PathVariable String userName) throws JsonProcessingException {
         return matchService.getStudent(userName);
     }
 
     @Transactional
     @DeleteMapping(path = "/delete/{userName}")
-    @CrossOrigin(origins={"http://localhost:3000", "https://match-swensational.netlify.app/"})
+    @CrossOrigin(origins={"http://localhost:3000", "https://match-swensational.netlify.app"})
     public  ResponseEntity deleteStudent(@PathVariable String userName){
         if (matchService.deleteStudent(userName)){
             return ResponseEntity.ok().build();
@@ -82,7 +82,7 @@ public class MatchController {
 
     @GetMapping(path = "/matches/{userName}")
     @ResponseBody
-    @CrossOrigin(origins={"http://localhost:3000", "https://match-swensational.netlify.app/"})
+    @CrossOrigin(origins={"http://localhost:3000", "https://match-swensational.netlify.app"})
     public List<Student> findBuddies(@PathVariable String userName) throws JsonProcessingException {
 
         //Student studentObject= getStudent(userName);
@@ -93,7 +93,7 @@ public class MatchController {
     }
     @PostMapping(path = "/update")
     @ResponseBody
-    @CrossOrigin(origins={"http://localhost:3000", "https://match-swensational.netlify.app/"})
+    @CrossOrigin(origins={"http://localhost:3000", "https://match-swensational.netlify.app"})
     public  ResponseEntity updateStudent(@RequestBody Student student){
         if (matchService.updateStudent(student)){
             return ResponseEntity.ok().build();
@@ -102,21 +102,21 @@ public class MatchController {
     }
 
     @GetMapping(path = "areMatched/{userName1}/{userName2}")
-    @CrossOrigin(origins={"http://localhost:3000", "https://match-swensational.netlify.app/"})
+    @CrossOrigin(origins={"http://localhost:3000", "https://match-swensational.netlify.app"})
     @ResponseBody
     public int matchExists(@PathVariable String userName1, @PathVariable String userName2){
         return matchService.matchExists(userName1, userName2);
     }
 
     @GetMapping(path = "findRequests/{userName}")
-    @CrossOrigin(origins={"http://localhost:3000", "https://match-swensational.netlify.app/"})
+    @CrossOrigin(origins={"http://localhost:3000", "https://match-swensational.netlify.app"})
     @ResponseBody
     public List<String> findRequests(@PathVariable String userName){
         return matchService.findRequests(userName);
     }
 
     @GetMapping(path = "findConfirmedMatches/{userName}")
-    @CrossOrigin(origins={"http://localhost:3000", "https://match-swensational.netlify.app/"})
+    @CrossOrigin(origins={"http://localhost:3000", "https://match-swensational.netlify.app"})
     @ResponseBody
     public List<String> findConfirmedMatches(@PathVariable String userName){
         return matchService.findConfirmedMatches(userName);
@@ -124,7 +124,7 @@ public class MatchController {
 
     @Transactional
     @PostMapping(path = "/addCourse")
-    @CrossOrigin(origins={"http://localhost:3000", "https://match-swensational.netlify.app/"})
+    @CrossOrigin(origins={"http://localhost:3000", "https://match-swensational.netlify.app"})
     public ResponseEntity addCourse(@RequestBody Course course){
         if(matchService.addCourse(course)) {
             return ResponseEntity.status(HttpStatus.CREATED).build();
@@ -136,21 +136,21 @@ public class MatchController {
 
     @GetMapping(path = "/getAllCourses")
     @ResponseBody
-    @CrossOrigin(origins={"http://localhost:3000", "https://match-swensational.netlify.app/"})
+    @CrossOrigin(origins={"http://localhost:3000", "https://match-swensational.netlify.app"})
     public List<Course> fetchAllCourses() {
         return matchService.getAllCourses();
     }
 
     @GetMapping(path = "/getCourseById/{id}")
     @ResponseBody
-    @CrossOrigin(origins={"http://localhost:3000", "https://match-swensational.netlify.app/"})
+    @CrossOrigin(origins={"http://localhost:3000", "https://match-swensational.netlify.app"})
     public Course getCourse(@PathVariable String id) throws JsonProcessingException {
         return matchService.getCourseById(id);
     }
 
     @Transactional
     @DeleteMapping(path = "/deleteCourse/{id}")
-    @CrossOrigin(origins={"http://localhost:3000", "https://match-swensational.netlify.app/"})
+    @CrossOrigin(origins={"http://localhost:3000", "https://match-swensational.netlify.app"})
     public  ResponseEntity deleteCourse(@PathVariable String id){
         if (matchService.deleteCourse(id)){
             return ResponseEntity.ok().build();
